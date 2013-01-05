ce50f94058eb92ba674b336a1eecf0e5be4e306b
David Quenault <davidquenault@hotmail.com>
2013-01-05 22:34:34 +0000

    Updated part1.rb

--- part1.rb
+++ part1.rb
@@ -8,7 +8,25 @@ def palindrome?(str)
 end
 
 def count_words(str)
-  # YOUR CODE HERE
+  # Create result hash (key value pair)
+  result = Hash.new
+  total = 0
+ 
+  # Scan input from file and add to hash 
+  str.scan(/\w+/i) {|w|
+	w.downcase!
+	if result.has_key?(w) then
+	  # Key exists so increment the counter
+	  result[w] = result[w] + 1
+	else
+	  # Key does not exist so add word to hash
+	  result.merge!({w => 1})
+	end
+	total = total + 1
+  }
+  # Print hash map
+  p result
+  return total
 end
 
 
@@ -23,13 +41,12 @@ else
   puts test_str + " is NOT a palindrome!"
 end
 
-
 test_str = "Madam, I'm Adam"
 
 if palindrome? test_str
   puts test_str " is a palindrome!"
 else
-  puts test_str " is NOT a palindrome!"
+  #puts test_str " is NOT a palindrome!"
 end
 
 
@@ -37,3 +54,9 @@ test_str = "The rent is due on the first day of the month unless the first day o
 
 word_count = count_words test_str
 puts word_count
+
+count_words("A man, a plan, a canal -- Panama")
+    # => {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}
+count_words "Doo bee doo bee doo"
+    # => {'doo' => 3, 'bee' => 2}
+
