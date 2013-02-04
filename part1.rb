55ebc6230bdc3e000e120796e9f2ba43d4461311
David Quenault <davidquenault@hotmail.com>
2013-02-04 21:22:40 +0000

    Completed part1.rb

--- part1.rb
+++ part1.rb
@@ -1,10 +1,5 @@
-#For those just starting out, one suggested way to work on your code is to have a command window open and a text editor with this
-#file loaded.  Make changes to this file and then run 'ruby part1.rb' at the command line, this will run your program.  Once you're
-#satisfied with your work, save your file and upload it to the checker.
-
 
 def palindrome?(str)
-<<<<<<< HEAD
   a = str.gsub(/\W/,"").downcase
   a == a.reverse 
 end
@@ -12,47 +7,9 @@ end
 def count_words(str)
   word_array = str.downcase.split(/\W/)
   result = Hash.new(0)
-  word_array.each { |x| result[x] =+ 1}
-  puts result
-end
-
-def count_words_old(str)
-=======
-  # YOUR CODE HERE
-  a = str.downcase.gsub(/\W/)
-  a == a.reverse
-end
-
-def count_words(str)
-
-array = Hash.new(0)
-
-
->>>>>>> 01e555a7f8790c406271e5165d288aefd0318dfd
-  # Create result hash (key value pair)
-  result = Hash.new
-  total = 0
- 
-  # Scan input from file and add to hash 
-  str.scan(/\w+/i) {|w|
-	w.downcase!
-	if result.has_key?(w) then
-	  # Key exists so increment the counter
-	  result[w] = result[w] + 1
-	else
-	  # Key does not exist so add word to hash
-	  result.merge!({w => 1})
-	end
-	total = total + 1
-  }
-  # Print hash map
+  word_array.each { |x| result[x] += 1}
   p result
-  return total
 end
-
-
-#the code below this line will test your functions.  You should remove everything below this line prior to submitting your file
-
 
 test_str = "there goes the neighborhood"
 
@@ -65,11 +22,9 @@ end
 test_str = "Madam, I'm Adam"
 
 if palindrome? test_str
-	p "blah blah blaa"
-#  puts test_str " is a palindrome!"
+  puts test_str + " is a palindrome!"
 else
-   puts " Not a palindrome"
-#  puts test_str " is NOT a palindrome!"
+  puts test_str + " is NOT a palindrome!"
 end
 
 
